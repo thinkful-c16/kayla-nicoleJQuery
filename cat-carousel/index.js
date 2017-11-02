0fac62b12ea1629a4d6d51d147b47910007ab48d
Kayla Webb <kaylarwebb@gmail.com>
2017-11-02 14:39:20 -0400

    completed first drill

--- cat-carousel/index.js
+++ cat-carousel/index.js
@@ -1,14 +1,26 @@
 'use strict'
 
-$('.thumbnail').click(function(event) {
-    // $('.hero').append($(event.currentTarget))
-   let newSource = $(event.currentTarget).find('img').attr('src');
-   let newAlt = $(event.currentTarget).find('img').attr('alt');
-   $('.hero img').attr({
-       alt: newAlt, 
-       src: newSource
-    });
+// $('.thumbnail').click(function(event) {
+//    let newSource = $(event.currentTarget).find('img').attr('src');
+//    let newAlt = $(event.currentTarget).find('img').attr('alt');
+//    $('.hero img').attr({
+//        alt: newAlt, 
+//        src: newSource
+//     });
 
-})
+// })
 
+//long version//
+// $(document).ready(function(){})
+
+$(function(){ 
+    $('.thumbnail').click(function(event) {
+        let newSource = $(event.currentTarget).find('img').attr('src');
+        let newAlt = $(event.currentTarget).find('img').attr('alt');
+        $('.hero img').attr({
+            alt: newAlt, 
+            src: newSource
+        });
+    })
+});
 
