d1884eac6e1597587922c47a1a50239097dd9ec6
Kayla Webb <kaylarwebb@gmail.com>
2017-11-02 17:04:36 -0400

    started shopping list app

---
+++ shoppinglist-challenge/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function addItems() {
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        let item = $('.js-shopping-list-entry').val();
+        $('.shopping-list').append(`<li><span class='shopping-item'>`+item+`</span>
+        <div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>`+'check'+`</span>
+            </button>
+            <button class="shopping-item-delete">
+                <span class="button-label">`+'delete'+`</span>
+            </button>
+            </div>
+            </li>`);
+        console.log(item);
+
+
+    })
+
+}
+
+$(addItems());
